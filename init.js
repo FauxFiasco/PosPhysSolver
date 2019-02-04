4fd1c260bb177df5e41eed0d1db2a8c2d39469dc
FoxyFiasco <47337975+FoxyFiasco@users.noreply.github.com>
2019-02-04 16:41:32 -0600

    Create init.js

---
+++ init.js
@@ -0,0 +1,1 @@
+javascript:$('head').append("<link rel='stylesheet' href='https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css'>");$('head').append("<script src='https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js'><\/script>");
